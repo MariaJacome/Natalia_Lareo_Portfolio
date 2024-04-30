b279ceb9bd3be169f9453ff15f40ae32db550f04
María Jácome Iglesias <mariajacomeiglesias@gmail.com>
2024-04-30 20:57:27 +0200

    Slider prueba - ajustar desplazamiento

--- js/work.js
+++ js/work.js
@@ -1,11 +1,39 @@
 'use strict'
 
-const carruselContainer = document.querySelector('.Carrusel-container')
+const carruselContainer = document.querySelector('.Carrusel-container--woty')
 const carruselBtn = document.querySelectorAll('.Carrusel-btn')
 const carruselImg = document.querySelectorAll('.Carrusel-img')
 
-
 let posicion = 0
+
+const moveContainer = () => {
+    carruselContainer.style.transform = `translateX(-${posicion * (100 / 9)}%)`
+}
+
+const limitarPosicion = () => {
+    posicion = Math.max(0, Math.min(posicion, numElementos - 1));
+};
+
+const carruselContainerHandler = e => {
+    if(e.deltaY > 0){
+        posicion++
+    }else{
+        posicion--
+    }
+    moveContainer()
+    limitarPosicion
+}
+
+const windowHandler = e => {
+    if( e.code === 'ArrowRight'){
+        posicion++
+    }else{
+        posicion--
+    }
+    moveContainer()
+    limitarPosicion()
+}
+
 
 let avanzarBtn = function(){
     carruselBtn.forEach( function( eachBtn , index ){
@@ -17,7 +45,10 @@ let avanzarBtn = function(){
 carruselBtn.forEach(function( eachBtn , index ){
     carruselBtn[index].addEventListener(`click` , function(){
         posicion = index
-        carruselContainer.style.transform = `translateX(-${posicion * (100 / 3)}%)`
+        moveContainer()
         avanzarBtn()
     })
 })
+
+carruselContainer.addEventListener(`mousewheel` , carruselContainerHandler)
+window.addEventListener(`keydown` , windowHandler)
