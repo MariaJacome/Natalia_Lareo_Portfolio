44f6562313ede03ce06ad1fc340c3b8c2ce649f5
María Jácome Iglesias <mariajacomeiglesias@gmail.com>
2024-05-01 11:16:56 +0200

    Ajustes slider - conflicto con el tamaño

--- js/work.js
+++ js/work.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const carruselContainer = document.querySelector('.Carrusel-container--woty')
-const carruselBtn = document.querySelectorAll('.Carrusel-btn')
 const carruselImg = document.querySelectorAll('.Carrusel-img')
 
 let posicion = 0
@@ -9,10 +8,6 @@ let posicion = 0
 const moveContainer = () => {
     carruselContainer.style.transform = `translateX(-${posicion * (100 / 9)}%)`
 }
-
-/*const limitarPosicion = () => {
-    posicion = Math.max(0, Math.min(posicion, numElementos - 1));
-};*/
 
 const carruselContainerHandler = e => {
     if(e.deltaY > 0){
@@ -33,22 +28,6 @@ const windowHandler = e => {
     moveContainer()
     limitarPosicion()
 }
-
-
-let avanzarBtn = function(){
-    carruselBtn.forEach( function( eachBtn , index ){
-        carruselBtn[index].classList.remove('isActive')    
-    })
-    carruselBtn[posicion].classList.add('isActive')
-}
-
-carruselBtn.forEach(function( eachBtn , index ){
-    carruselBtn[index].addEventListener(`click` , function(){
-        posicion = index
-        moveContainer()
-        avanzarBtn()
-    })
-})
 
 carruselContainer.addEventListener(`mousewheel` , carruselContainerHandler)
 window.addEventListener(`keydown` , windowHandler)
