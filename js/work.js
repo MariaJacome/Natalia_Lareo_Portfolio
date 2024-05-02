5c3eebfa2ecceeeecdb9b773b39db2e5c3aa0b2b
María Jácome Iglesias <mariajacomeiglesias@gmail.com>
2024-05-02 20:36:17 +0200

    Mejora js carruseles desktop + Prueba responsive mobile

--- js/work.js
+++ js/work.js
@@ -1,44 +1,78 @@
 'use strict'
 
-const carrusel = document.querySelector('.Carrusel')
-const carruselContainer = document.querySelector('.Carrusel-container')
+const carruselList = document.querySelectorAll('.Carrusel')
+const carruselContainerList = document.querySelectorAll('.Carrusel-container')
 
-carrusel.addEventListener(`mouseover` , function (){
-    document.body.style.overflowY = "hidden";
-})
+const tocando = false;
 
-carrusel.addEventListener(`mouseout` , function (){
-    document.body.style.overflowY = "auto";
-})
+const touchStart = (e) => {
+    tocando = true;
+}
 
-let pixels = 0
-let increase = 100
+const touchMove = (e) => {
+    tocando = true;
+}
+
+const touchEnd = (e) => {
+    tocando = false;
+}
 
 
-carrusel.addEventListener(`mousewheel` , function (e){
-    const carruselContainerWidth = carruselContainer.getBoundingClientRect().width
-    const windowWidth = window.innerWidth
-    const difference = carruselContainerWidth - windowWidth
+carruselList.forEach (( eachCarrusel , index ) => {   
+    
+    const carrusel = carruselList[index]
+    const carruselContainer = carruselContainerList[index]
+    
+    carrusel.addEventListener(`mouseover` , function (){
+        document.body.style.overflowY = "hidden";
+    })
 
-    //Comprobamos direccion del desplazamiento
-    if(e.deltaY > 0 && pixels < difference){
-        //Desplazamiento es igual a posicion actual (pixels) mas el incremento definido
-        pixels = pixels + increase
+    carrusel.addEventListener(`mouseout` , function () {
+        document.body.style.overflowY = "auto";
+    })
 
-        //Si despues de calcular el desplazamiento, nos hemos pasado del ancho del carrusel (diference), establecemos la posicion a la posicion máxima (diference)
-        if(pixels > difference){
-            pixels = difference
+    let pixels = 0
+    let increase = 100
+
+
+    carrusel.addEventListener(`mousewheel` , function (e){
+        const carruselContainerWidth = carruselContainer.getBoundingClientRect().width
+        const windowWidth = window.innerWidth
+        const difference = carruselContainerWidth - windowWidth
+
+        //Comprobamos direccion del desplazamiento
+        if(e.deltaY > 0 && pixels < difference){
+            //Desplazamiento es igual a posicion actual (pixels) mas el incremento definido
+            pixels = pixels + increase
+
+            //Si despues de calcular el desplazamiento, nos hemos pasado del ancho del carrusel (diference), establecemos la posicion a la posicion máxima (diference)
+            if(pixels > difference){
+                pixels = difference
+            }
+        }else if (e.deltaY < 0 && pixels > 0){       
+            //Desplazamiento es igual a posicion actual (pixels) menos el incremento definido
+            pixels = pixels - increase
+            
+            //Si despues de calcular el desplazamiento, nos hemos pasado a una posicion negativa, establecemos la posicion a 0, la posicion mínima
+            if(pixels < 0){
+                pixels = 0
+            }
         }
-    }else if (e.deltaY < 0 && pixels > 0){       
-        //Desplazamiento es igual a posicion actual (pixels) menos el incremento definido
-        pixels = pixels - increase
-        
-        //Si despues de calcular el desplazamiento, nos hemos pasado a una posicion negativa, establecemos la posicion a 0, la posicion mínima
-        if(pixels < 0){
-            pixels = 0
-        }
-    }
-    //Efectuamos el desplazamiento proviamente calculado
-    carruselContainer.style.transform = `translateX(-${pixels}px)`
-})
+        //Efectuamos el desplazamiento proviamente calculado
+        carruselContainer.style.transform = `translateX(-${pixels}px)`
+    })
+
+    /*carrusel.addEventListener('touchstart', touchStart)
+    carrusel.addEventListener('touchmove', touchMove)
+    carrusel.addEventListener('touchend', touchEnd)*/
+});
+
+
+
+
+
+
+
+
+
 
