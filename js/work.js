714a4f7b9e57eefa0f680eaff801c920ee790f0e
María Jácome Iglesias <mariajacomeiglesias@gmail.com>
2024-05-03 00:41:35 +0200

    primera prueba drag and move

--- js/work.js
+++ js/work.js
@@ -3,22 +3,10 @@
 const carruselList = document.querySelectorAll('.Carrusel')
 const carruselContainerList = document.querySelectorAll('.Carrusel-container')
 
-const tocando = false;
-
-const touchStart = (e) => {
-    tocando = true;
-}
-
-const touchMove = (e) => {
-    tocando = true;
-}
-
-const touchEnd = (e) => {
-    tocando = false;
-}
 
 
-carruselList.forEach (( eachCarrusel , index ) => {   
+
+carruselList.forEach (( eachCarrusel, index ) => {   
     
     const carrusel = carruselList[index]
     const carruselContainer = carruselContainerList[index]
@@ -31,40 +19,51 @@ carruselList.forEach (( eachCarrusel , index ) => {
         document.body.style.overflowY = "auto";
     })
 
-    let pixels = 0
-    let increase = 100
-
-
-    carrusel.addEventListener(`mousewheel` , function (e){
+    let posicionActualCarrusel = 0
+    let desplazarCarrusel = function(incremento, direccion)
+    {
         const carruselContainerWidth = carruselContainer.getBoundingClientRect().width
         const windowWidth = window.innerWidth
         const difference = carruselContainerWidth - windowWidth
 
-        //Comprobamos direccion del desplazamiento
-        if(e.deltaY > 0 && pixels < difference){
+        //Comprobamos direccion del desplazamiento, positivo hacia la derecha, negativo hacia la izquierda
+        if(direccion && posicionActualCarrusel < difference){
             //Desplazamiento es igual a posicion actual (pixels) mas el incremento definido
-            pixels = pixels + increase
+            posicionActualCarrusel = posicionActualCarrusel + incremento
 
             //Si despues de calcular el desplazamiento, nos hemos pasado del ancho del carrusel (diference), establecemos la posicion a la posicion máxima (diference)
-            if(pixels > difference){
-                pixels = difference
+            if(posicionActualCarrusel > difference){
+                posicionActualCarrusel = difference
             }
-        }else if (e.deltaY < 0 && pixels > 0){       
+        }else if (!direccion && posicionActualCarrusel > 0){       
             //Desplazamiento es igual a posicion actual (pixels) menos el incremento definido
-            pixels = pixels - increase
+            posicionActualCarrusel = posicionActualCarrusel - incremento
             
             //Si despues de calcular el desplazamiento, nos hemos pasado a una posicion negativa, establecemos la posicion a 0, la posicion mínima
-            if(pixels < 0){
-                pixels = 0
+            if(posicionActualCarrusel < 0){
+                posicionActualCarrusel = 0
             }
         }
         //Efectuamos el desplazamiento proviamente calculado
-        carruselContainer.style.transform = `translateX(-${pixels}px)`
+        carruselContainer.style.transform = `translateX(-${posicionActualCarrusel}px)`
+    }
+
+
+    
+    let incrementoScroll = 200
+    carrusel.addEventListener(`mousewheel` , function (e){
+        desplazarCarrusel(incrementoScroll, e.deltaY > 0);
     })
 
-    /*carrusel.addEventListener('touchstart', touchStart)
-    carrusel.addEventListener('touchmove', touchMove)
-    carrusel.addEventListener('touchend', touchEnd)*/
+    let incrementoTouch = 1
+    let ultimoX
+    carrusel.addEventListener('touchstart', function(e){        
+        ultimoX = e.originalEvent.touches[0].clientX;   
+    })
+    carrusel.addEventListener('touchmove', function(e){        
+        actualX = e.originalEvent.touches[0].clientX;
+        desplazarCarrusel(incrementoTouch, actualX>ultimoX);   
+    })
 });
 
 
